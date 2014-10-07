dfd8d23cfdc011b573f3b5a45d40ae74e3b8fd5b
Ade Imam Kurniawan <ade.imam@samsung.com>
2014-10-07 18:14:52 +0700

    add top coder 2nd solution to problem 06122014

---
+++ src/main/java/topcoder/FoxAndGame.java
@@ -0,0 +1,14 @@
+package topcoder;
+
+public class FoxAndGame {
+
+	public int countStars(String[] results) {
+
+		int stars = 0;
+		for (int i = 0; i < results.length; i++) {			
+			stars += results[i].replace("-", "").length();
+		}
+
+		return stars;
+	}
+}
