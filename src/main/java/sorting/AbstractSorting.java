3bd996ec73729202e5e83a549825da0772fd8d03
Ade Imam Kurniawan <ade.imam@samsung.com>
2014-10-09 17:33:48 +0700

    fix selection sort and add insertion sort

--- src/main/java/sorting/AbstractSorting.java
+++ src/main/java/sorting/AbstractSorting.java
@@ -6,43 +6,48 @@ import java.io.IOException;
 
 public abstract class AbstractSorting {
 
-	public abstract void sort(Comparable[] a);
+    public abstract void sort(Comparable[] a);
 
-	public boolean less(Comparable v, Comparable w) {
-		return v.compareTo(w) < 0;
-	}
+    public boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
 
-	public void exch(Comparable[] a, int i, int j) {
-		Comparable t = a[i]; a[i] = a[j]; a[j] = t;
-	}
+    public void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i]; a[i] = a[j]; a[j] = t;
+    }
 
-	public void show(Comparable[] a) {
-		
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
-		
-		System.out.println();
-	}
+    public void show(Comparable[] a, long startTime, long endTime) {
 
-	public boolean isSorted(Comparable[] a) {
-		
-		for (int i = 1; i < a.length; i++) {
-			if (less(a[i], a[i-1])) { return false; }
-		}
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
 
-		return true;
-	}
+        System.out.println();
+        System.out.println("Execution time: " + (endTime - startTime) + "s");
+    }
 
-	public void execute(String[] args) {
-		try {
-			String[] a = new BufferedReader(new InputStreamReader(System.in)).readLine().trim().split(" ");
-			sort(a);
-			if (isSorted(a)) {
-				show(a);
-			}
-		} 
-		catch (IllegalArgumentException e) {}
-		catch (IOException e) {}
-	}
+    public boolean isSorted(Comparable[] a) {
+
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) { return false; }
+        }
+
+        return true;
+    }
+
+    public void execute(String[] args) {
+        try {
+            String[] a = new BufferedReader(new InputStreamReader(System.in)).readLine().trim().split(" ");
+
+            final long startTime = System.currentTimeMillis();
+            sort(a);
+            final long endTime = System.currentTimeMillis();
+
+            if (isSorted(a)) {
+                show(a, startTime, endTime);
+            }
+        }
+        catch (IllegalArgumentException e) {}
+        catch (IOException e) {}
+    }
 }
--- src/main/java/sorting/SelectionSort.java
+++ src/main/java/sorting/SelectionSort.java
@@ -2,18 +2,23 @@ package sorting;
 
 public class SelectionSort extends AbstractSorting {
 
-	public void sort(Comparable[] a) {
-		// Sort a[] into incresing order
-		int N = a.length;
-		for (int i = 1; i < N; i++) {
-			// insert a[i] among a[i-1], a[i-2], ...
-			for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
-				exch(a, j, j-1);
-			}
-		}
-	}
+    public void sort(Comparable[] a) {
+        // Sort a[] into incresing order
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            // Exchange a[i] with smallest entry in a[i+1...N]
+            int min = i; // index of smallest entry
+            for (int j = i+1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            // Exchange it
+            exch(a, i, min);
+        }
+    }
 
-	public static void main(String[] args) {
-		new SelectionSort().execute(args);
-	}
+    public static void main(String[] args) {
+        new SelectionSort().execute(args);
+    }
 }
